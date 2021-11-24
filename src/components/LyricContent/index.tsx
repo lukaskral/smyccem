1df3974fab70041f24b222bab07b74d6fc57e685
Tomas Randus <tom.randus@gmail.com>
2021-11-24 12:08:17 +0100

    dur / moll

--- src/components/LyricContent/index.tsx
+++ src/components/LyricContent/index.tsx
@@ -12,6 +12,12 @@ const LyricContent = ({content, title}: LyricContentProps) => {
         <Page title={title} bg="/assets/site/001.jpeg">
             <h1>{title}</h1>
             <div className={styles['lyric-content']} dangerouslySetInnerHTML={{__html: content}} />
+            <br />
+            <hr />
+
+            <em style={{color: 'white', fontStyle: 'italic'}}>
+                (Malá písmena značí akordy <b>Moll</b>, velká <b>Dur</b>)
+            </em>
         </Page>
     );
 };
