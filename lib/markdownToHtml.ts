4fa240e8266056261525b285f1a90a08cb2e4d74
Tomas Randus <tom.randus@gmail.com>
2022-08-30 17:24:11 +0200

    fix types

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,13 +1,13 @@
-import remark from "remark";
-import html from "remark-html";
+import {remark} from 'remark';
+import html from 'remark-html';
 // @ts-ignore
-import oembed from "remark-oembed";
+import oembed from 'remark-oembed';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark()
-    .use(oembed)
-    // .use(chords)
-    .use(html)
-    .process(markdown);
-  return result.toString();
+    const result = await remark()
+        .use(oembed)
+        // .use(chords)
+        .use(html)
+        .process(markdown);
+    return result.toString();
 }
