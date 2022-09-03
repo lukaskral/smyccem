9e9f697b36201a487140d6df694f2955f2cf36c8
Tomas Randus <tom.randus@gmail.com>
2022-09-03 15:27:14 +0200

    add new video

--- src/pages/hudba.tsx
+++ src/pages/hudba.tsx
@@ -6,6 +6,16 @@ const Music = () => {
             <iframe
                 width="560"
                 height="315"
+                src="https://www.youtube.com/embed/iHll1Ynj9rg"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+            <br /> <br />
+            <iframe
+                width="560"
+                height="315"
                 src="https://www.youtube.com/embed/y0TGWSv40Cw"
                 title="YouTube video player"
                 frameBorder="0"
