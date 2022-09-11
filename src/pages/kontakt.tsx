a9801134aa0587ca349a96ea419b7437885f5068
Tomas Randus <tom.randus@gmail.com>
2022-09-11 21:46:33 +0200

    Instagram link

--- src/pages/kontakt.tsx
+++ src/pages/kontakt.tsx
@@ -6,12 +6,17 @@ const Contact = () => {
         <Page title="Kontakt" bg="/assets/site/002.jpeg" contentAlign="bottom">
             <p>tom.randus@gmail.com</p>
             <p>+420 724 023 901</p>
+            <br />
             <p>
                 <a href="https://www.facebook.com/Trhni-si-smy%C4%8Dcem-100603462205271">
                     Facebook
                 </a>
             </p>
-
+            <br />
+            <p>
+                <a href="https://www.instagram.com/smyccem/?hl=cs">Instagram</a>
+            </p>
+            <br />
             {/* <h2>Zvučení</h2> */}
             <p>
                 Pokud je to možné, hrajeme bez jakéhokoliv dozvučení, v prostorech jako jsou budovy
