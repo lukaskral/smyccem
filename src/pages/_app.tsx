87dbc66d7af814aa6455ab2bda53be9001c9a83e
Tomas Randus <tom.randus@gmail.com>
2022-10-21 13:39:34 +0200

    GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
+import {useEffect} from 'react';
+import Script from 'next/script';
+import {useRouter} from 'next/router';
+import {GTM_ID, pageview} from '../utils/gtm';
 import {AppProps} from 'next/app';
-import '../styles/global.scss';
 
-export default function MyApp({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+    const router = useRouter();
+    useEffect(() => {
+        router.events.on('routeChangeComplete', pageview);
+        return () => {
+            router.events.off('routeChangeComplete', pageview);
+        };
+    }, [router.events]);
+
+    return (
+        <>
+            {/* Google Tag Manager - Global base code */}
+            <Script
+                id="gtag-base"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+                }}
+            />
+            <Component {...pageProps} />
+        </>
+    );
 }
+
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,19 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document';
-
+import {GTM_ID} from '../utils/gtm';
 export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="cs">
                 <Head />
                 <body>
+                    <noscript>
+                        <iframe
+                            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                            height="0"
+                            width="0"
+                            style={{display: 'none', visibility: 'hidden'}}
+                        />
+                    </noscript>
                     <Main />
                     <NextScript />
                 </body>
